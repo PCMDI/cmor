05b35aab3273e4e88d4a4a62fc597380f939e7a3
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-08-26 12:42:16 -0700

    delete create output path

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -3,7 +3,7 @@ from cmor_const import *
 from pywrapper import (
     CMORError, axis, variable, write, setup, load_table, set_table, zfactor,
     close, grid, set_grid_mapping, time_varying_grid_coordinate, dataset_json,
-    set_cur_dataset_attribute, get_cur_dataset_attribute, create_output_path,
+    set_cur_dataset_attribute, get_cur_dataset_attribute, 
     has_cur_dataset_attribute, set_variable_attribute, get_variable_attribute,
     has_variable_attribute, get_final_filename, set_deflate)
 
