1c6ca42f18d1b086c65b585bf77f1f7207e966c9
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-08-26 15:39:53 -0700

    Work with new TERM signal exception.

--- Test/test_python_CMIP6_CV_baddirectory.py
+++ Test/test_python_CMIP6_CV_baddirectory.py
@@ -11,8 +11,6 @@
 #   3. Download
 #      https://github.com/PCMDI/cmor/blob/master/Test/<filename>.json
 #      to the 'Test/' directory.
-
-from threading import Thread
 import time
 import cmor
 import numpy
@@ -21,7 +19,6 @@ import signal
 import sys
 import os
 import tempfile
-
 
 # ------------------------------------------------------
 # Copy stdout and stderr file descriptor for cmor output
@@ -36,35 +33,21 @@ os.dup2(tmpfile[0], 1)
 os.dup2(tmpfile[0], 2)
 os.close(tmpfile[0])
 
-global testOK
-testOK = []
 
-# --------------
-# Create thread
-# --------------
+def run():
+    unittest.main()
 
-
-def sig_handler(signum, frame):
-
-    global testOK
-
-    f = open(tmpfile[1], 'r')
-    lines = f.readlines()
+def getAssertTest():
+    f=open(tmpfile[1],'r')
+    lines=f.readlines()
     for line in lines:
         if line.find('Error:') != -1:
             testOK = line.strip()
             break
     f.close()
-#    os.unlink(tmpfile[1])
-
-
-def run():
-    unittest.main()
-
-signal.signal(signal.SIGINT, sig_handler)
-signal.signal(signal.SIGINT, sig_handler)
-
-
+    os.unlink(tmpfile[1])
+    return testOK
+    
 class TestdirectoryMethods(unittest.TestCase):
 
     def test_Directory(self):
@@ -72,19 +55,17 @@ class TestdirectoryMethods(unittest.TestCase):
         # -------------------------------------------
         # Try to call cmor with a bad institution_ID
         # -------------------------------------------
-        global testOK
         error_flag = cmor.setup(inpath='Tables', netcdf_file_action=cmor.CMOR_REPLACE)
-        error_flag = cmor.dataset_json("Test/test_python_CMIP6_CV_baddirectory.json")
-
-        os.dup2(newstdout, 1)
-        os.dup2(newstderr, 2)
-        sys.stdout = os.fdopen(newstdout, 'w', 0)
-        time.sleep(1)
-        self.assertIn("unable to create this directory", testOK)
+        try:
+            error_flag = cmor.dataset_json("Test/test_python_CMIP6_CV_baddirectory.json")
+        except BaseException as err:
+            testOK = getAssertTest()
+            os.dup2(newstdout, 1)
+            os.dup2(newstderr, 2)
+            sys.stdout = os.fdopen(newstdout, 'w', 0)
+            sys.stderr = os.fdopen(newstderr, 'w', 0)
+            self.assertIn("unable to create this directory", testOK)
 
 
 if __name__ == '__main__':
-    t = Thread(target=run)
-    t.start()
-    while t.is_alive():
-        t.join(1)
+    run()
