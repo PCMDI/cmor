42a7d6064312701a7299ef385a5889425b97d13a
Emma Hogan <ehogan@users.noreply.github.com>
2016-10-11 15:36:36 +0100

    Fix rookie mistake! (strip strips all combinations of the values provided)

--- Test/all_tests.py
+++ Test/all_tests.py
@@ -5,7 +5,7 @@ def create_test_suite():
     test_file_strings = glob.glob('Test/test_python_CMIP6_CV*.py')
     test_file_strings.extend(['Test/test_python_has_cur_dataset_attribute.py',
                               'Test/test_python_has_variable_attribute.py'])
-    module_strings = [test_file_string.replace('/', '.').strip('.py')
+    module_strings = [os.path.splitext(test_file_string)[0].replace('/', '.')
                       for test_file_string in test_file_strings]
     suites = [unittest.defaultTestLoader.loadTestsFromName(module_string)
               for module_string in module_strings]
