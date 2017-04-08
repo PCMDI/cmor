77e73dca292a0a98a12397716331416fbcbf1e51
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-04-07 17:16:04 -0700

    add numpy 1.12 to anaconda upload

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -25,11 +25,12 @@ cd conda-recipes
 cmorversion=`echo $1 | cut -d- -f2`
 python ./prep_for_build.py -v `date +%Y.%m.%d`.${cmorversion} -b $1
 echo "Building now"
+conda build -c conda-forge -c uvcdat/label/nightly -c uvcdat --numpy=1.12 cmor
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`.${cmorversion}-np112py27_0.tar.bz2 --force
 conda build -c conda-forge -c uvcdat/label/nightly -c uvcdat --numpy=1.11 cmor
-echo "Uploading"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`.${cmorversion}-np111py27_0.tar.bz2 --force
-conda build cmor -c conda-forge -c uvcdat --numpy=1.10
+conda build -c conda-forge  -c uvcdat/label/nightly -c uvcdat --numpy=1.10 cmor
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`.${cmorversion}-np110py27_0.tar.bz2 --force
-conda build cmor -c conda-forge -c uvcdat --numpy=1.9
+conda build -c conda-forge  -c uvcdat/label/nightly -c uvcdat --numpy=1.9 cmor
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`.${cmorversion}-np19py27_0.tar.bz2 --force
 
