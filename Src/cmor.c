4d4d182f53ef5565cf2a52874736e9cb9116dc5c
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-07-25 16:36:34 -0700

    add star to conda_upload filename

--- Src/cmor.c
+++ Src/cmor.c
@@ -2844,11 +2844,14 @@ int cmor_setGblAttr(int var_id)
         ierr += cmor_CV_setInstitution(cmor_tables[nVarRefTblID].CV);
     }
 
+    if (cmor_has_cur_dataset_attribute(GLOBAL_ATT_FURTHERINFOURLTMPL) == 0) {
+        ierr += cmor_CV_checkFurtherInfoURL(nVarRefTblID);
+    }
+
     if (cmor_has_cur_dataset_attribute(GLOBAL_IS_CMIP6) == 0) {
         ierr += cmor_CV_checkSourceID(cmor_tables[nVarRefTblID].CV);
         ierr += cmor_CV_checkExperiment(cmor_tables[nVarRefTblID].CV);
         //ierr += cmor_CV_checkGrids(cmor_tables[nVarRefTblID].CV);
-        ierr += cmor_CV_checkFurtherInfoURL(nVarRefTblID);
         ierr += cmor_CV_checkParentExpID(cmor_tables[nVarRefTblID].CV);
         ierr += cmor_CV_checkSubExpID(cmor_tables[nVarRefTblID].CV);
     }
