489ba836c52870872ee4a710f85245eea4b6b33c
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-09-14 14:37:25 -0700

    add recipes and env yaml file

---
+++ recipes/cmor/build.sh
@@ -0,0 +1,32 @@
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+CONDA_LST=`conda list`
+if [[ ${CONDA_LST}'y' == *'openmpi'* ]]; then
+    export CC=mpicc
+    export CXX=mpicxx
+    export LC_RPATH="${PREFIX}/lib"
+    export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+fi
+
+
+./configure \
+    --with-python=${PREFIX}   \
+    --with-uuid=${PREFIX} \
+    --with-udunits2=${PREFIX} \
+    --with-netcdf=${PREFIX} \
+    --with-libjson-c=${PREFIX} \
+    --prefix=${PREFIX}
+make
+make install
+# Make sure CMOR UDNITS2 env is still present in the package
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/cmor-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/cmor-deactivate.sh
+## END BUILD
+
