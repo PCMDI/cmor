501857b4e6a356c5bbeb8f846eac1cb0087bf9f5
Charles Doutriaux <doutriaux1@llnl.gov>
2018-09-06 10:25:49 -0700

    new script for cron job

--- scripts/CMIP6_CV_cron_github.sh
+++ scripts/CMIP6_CV_cron_github.sh
@@ -1,27 +1,13 @@
 #!/bin/bash -x
-export PATH=~/anaconda2/bin:/usr/local/bin:/usr/bin:/bin
-alias git='hub'
-cd ~/cmor/scripts
-python createCMIP6CV.py
-# -----
-cd ~/cmip6-cmor-tables/Tables
-git checkout master
-git pull --all -p
-git checkout CMIP6_CVupdt
+export REPOS_PATH=${1:-"${HOME}/CVs_tmp"}
+echo "Assuming REPOs cloned in "${REPOS_PATH}
+cd ${REPOS_PATH}/cmor/scripts
 git pull
-git merge master
-git push 
-cp -v ~/cmor/scripts/CMIP6_CV.json  .
+python createCMIP6CV.py
+cd ${REPOS_PATH}/cmip6-cmor-tables/Tables
+git pull
+cp -v ${REPOS_PATH}/cmor/scripts/CMIP6_CV.json  .
 msg="cron: update CMIP6_CV -- "`date +%Y-%m-%dT%H:%M`
 echo $msg
-git commit -am $msg
+git commit -am '$msg'
 git push
-URL=`hub pull-request CMIP6_CVupdt -m $msg`
-if [[ ! -z $URL ]]; then
-	PR=`echo $URL | awk -F/ '{print $7}'`
-	TOKEN=`cat ~/.config/hub  | grep oauth_token | awk -F: '{print $2}' | sed -e 's/^\s*//'`
-	curl "https://api.github.com/repos/pcmdi/cmip6-cmor-tables/pulls/$PR/merge"   -XPUT   -H "Authorization: token $TOKEN"   -H "Accept: application/vnd.github.polaris-preview"   -d '{ "commit_message":"$msg", "squash": true, "commit_title": "$msg" }'
-	git checkout master
-	git pull origin
-fi
-#
