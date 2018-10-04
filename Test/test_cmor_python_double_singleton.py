b0815e0054c5c67ce3d4702f6f9c1af24d329519
Chris Mauzey <mauzey1@llnl.gov>
2018-10-04 15:05:28 -0700

    Adding more tests

---
+++ Test/test_cmor_python_double_singleton.py
@@ -0,0 +1,49 @@
+import common
+import cmor
+import os
+import unittest
+import base_test_cmor_python
+
+
+class TestCase(base_test_cmor_python.BaseCmorTest):
+
+    def testDoubleSingleton(self):
+        try:
+            cmor.setup(inpath=self.testdir,
+                    netcdf_file_action=cmor.CMOR_REPLACE,
+                    logfile=self.logfile)
+
+            cmor.dataset_json(os.path.join(self.testdir, "common_user_input.json"))
+            cmor.load_table(os.path.join(self.tabledir, 'CMIP6_6hrLev.json'))
+            itim, ilat, ilon = common.read_cmor_time_lat_lon()
+
+            ilambda = cmor.axis(
+                table_entry='lambda550nm',
+                units='m',
+                length=1,
+                coord_vals=[550.,],
+                cell_bounds=[500.,600.])
+            # Creates two singleton dims?
+
+            varid = cmor.variable(table_entry=common.specs["BS"]["entry"],
+                                units=common.specs["BS"]["units"],
+                                axis_ids=[itim, ilat, ilon, ilambda],
+                                missing_value=1.e28,
+                                positive=common.specs["BS"]["positive"],
+                                original_name="BS")
+            for index in range(2):
+                tim_array, bnds_tim = common.read_time(index)
+                data = common.read_2d_input_files(index, "BS", (common.lat, common.lon))
+                print data.shape, data
+                print tim_array, bnds_tim
+                cmor.write(var_id=varid, data=data, ntimes_passed=1,
+                        time_vals=tim_array, time_bnds=bnds_tim)
+                print("Passed write")
+            cmor.close()
+            self.processLog()
+        except BaseException:
+            raise
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Test/test_cmor_python_half_levels.py
@@ -0,0 +1,78 @@
+import cdms2
+import cmor
+import numpy
+import os
+import unittest
+import base_test_cmor_python
+import common
+
+
+class TestCase(base_test_cmor_python.BaseCmorTest):
+
+    def testHalfLevels(self):
+        try:
+            varin3d = ["MC", ]
+
+            n3d = len(varin3d)
+
+            cmor.setup(inpath=self.testdir,
+                    netcdf_file_action=cmor.CMOR_REPLACE,
+                    logfile=self.logfile)
+
+            cmor.dataset_json(os.path.join(self.testdir, "common_user_input.json"))
+            ierr = cmor.load_table(os.path.join(self.tabledir, 'CMIP6_Amon.json'))
+            itim, ilat, ilon = common.read_cmor_time_lat_lon()
+
+            ilev_half = cmor.axis(table_entry='standard_hybrid_sigma_half',
+                                units='1',
+                                coord_vals=common.zlevs)
+            print("ILEVL half:", ilev_half)
+
+            cmor.zfactor(zaxis_id=ilev_half, zfactor_name='p0',
+                        units='hPa', zfactor_values=common.p0)
+            print "p0 1/2"
+            cmor.zfactor(zaxis_id=ilev_half, zfactor_name='b_half', axis_ids=[ilev_half, ],
+                        zfactor_values=common.b_coeff)
+            print "b 1/2"
+            cmor.zfactor(zaxis_id=ilev_half, zfactor_name='a_half', axis_ids=[ilev_half, ],
+                        zfactor_values=common.a_coeff)
+            print "a 1/2"
+            ps_var = cmor.zfactor(zaxis_id=ilev_half, zfactor_name='ps',
+                                axis_ids=[ilon, ilat, itim], units='hPa')
+            print "ps 1/2"
+
+            var3d_ids = []
+            for m in varin3d:
+                print "3d VAR:", m
+                var3d_ids.append(
+                    cmor.variable(table_entry=common.specs[m]["entry"],
+                                units=common.specs[m]["units"],
+                                axis_ids=[itim, ilev_half, ilat, ilon],
+                                missing_value=1.e28,
+                                positive=common.specs[m]["positive"],
+                                original_name=m)
+                )
+            print "Ok now writing", var3d_ids, common.ntimes
+            for index in range(common.ntimes):
+                tim_array, bnds_tim = common.read_time(index)
+                for i, varname in enumerate(varin3d):
+                    data = common.read_3d_input_files(
+                        index, varname, (common.lev, common.lat, common.lon))
+                    print data.shape, data
+                    print tim_array, bnds_tim
+                    cmor.write(var_id=var3d_ids[i], data=data, ntimes_passed=1,
+                            time_vals=tim_array, time_bnds=bnds_tim)
+                    print("Passed write")
+                    # PSURF
+                    data = common.read_2d_input_files(
+                        index, "PSURF", (common.lat, common.lon))
+                    cmor.write(var_id=ps_var, data=data, ntimes_passed=1,
+                            time_vals=tim_array, time_bnds=bnds_tim, store_with=var3d_ids[i])
+            cmor.close()
+            self.processLog()
+        except BaseException:
+            raise
+
+
+if __name__ == '__main__':
+    unittest.main()
