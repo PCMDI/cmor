a8a1f848eed65fd1683df19b19806d4ca78c31cb
mauzey1 <mauzey1@llnl.gov>
2019-06-21 17:54:27 -0700

    Add test_cmor_python_not_enough_times_written.py

---
+++ Test/test_cmor_python_not_enough_times_written.py
@@ -0,0 +1,58 @@
+import cmor
+import numpy
+import unittest
+import sys
+import os
+import tempfile
+import re
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self, *args, **kwargs):
+        # create temporary log file
+        self.logfile = tempfile.mkstemp()[1]
+
+    def tearDown(self):
+        os.unlink(self.logfile)
+
+    def testNotEnoughTimesWritten(self):
+        try:
+
+            cmor.setup(inpath='Tables', netcdf_file_action=cmor.CMOR_REPLACE, logfile=self.logfile)
+            cmor.dataset_json("Test/CMOR_input_example.json")
+            cmor.set_cur_dataset_attribute(
+                "source_type", "AOGCM ISM")
+
+            cmor.load_table("CMIP6_Omon.json")
+            itime = cmor.axis(table_entry="time", units='months since 2010',
+                              coord_vals=numpy.array([0, 1, 2, 3, 4.]),
+                              cell_bounds=numpy.array([0, 1, 2, 3, 4, 5.]))
+            ivar = cmor.variable(
+                table_entry="masso",
+                axis_ids=[itime],
+                units='kg')
+
+            data_len = 5
+            written_len = 4
+            data = numpy.random.random(data_len)
+            for i in range(0, written_len):
+                a = cmor.write(ivar, data[i:i+1], ntimes_passed=1)
+                
+            cmor.close()
+
+            # find "not enough times written" warning in log
+            warn_msg = "! Warning: while closing variable 0 (masso, table Omon)\n" \
+                       "! we noticed you wrote 4 time steps for the variable,\n" \
+                       "! but its time axis 0 (time) has 5 time steps"
+            remove_ansi = re.compile(r'\x1b\[[0-?]*[ -/]*[@-~]')
+            with open(self.logfile) as f:
+                lines = f.readlines()
+                log_text = remove_ansi.sub('', ''.join(lines))
+                self.assertIn(warn_msg, log_text)
+
+        except BaseException:
+            raise
+
+
+if __name__ == '__main__':
+    unittest.main()
