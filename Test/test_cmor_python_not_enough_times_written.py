2f6cb7d0ff7b305541793290cc4af432910e515b
mauzey1 <mauzey1@llnl.gov>
2019-06-24 15:41:28 -0700

    Add time_len variable to test_cmor_python_not_enough_times_written.py

--- Test/test_cmor_python_not_enough_times_written.py
+++ Test/test_cmor_python_not_enough_times_written.py
@@ -18,6 +18,10 @@ class TestCase(unittest.TestCase):
     def testNotEnoughTimesWritten(self):
         try:
 
+            time_len = 5
+            time_vals = numpy.arange(time_len)
+            time_bnds = numpy.arange(time_len+1)
+
             cmor.setup(inpath='Tables', netcdf_file_action=cmor.CMOR_REPLACE, logfile=self.logfile)
             cmor.dataset_json("Test/CMOR_input_example.json")
             cmor.set_cur_dataset_attribute(
@@ -25,25 +29,25 @@ class TestCase(unittest.TestCase):
 
             cmor.load_table("CMIP6_Omon.json")
             itime = cmor.axis(table_entry="time", units='months since 2010',
-                              coord_vals=numpy.array([0, 1, 2, 3, 4.]),
-                              cell_bounds=numpy.array([0, 1, 2, 3, 4, 5.]))
+                              coord_vals=time_vals,
+                              cell_bounds=time_bnds)
             ivar = cmor.variable(
                 table_entry="masso",
                 axis_ids=[itime],
                 units='kg')
 
-            data_len = 5
-            written_len = 4
+            data_len = time_len - 1
             data = numpy.random.random(data_len)
-            for i in range(0, written_len):
+            for i in range(0, data_len):
                 a = cmor.write(ivar, data[i:i+1], ntimes_passed=1)
                 
             cmor.close()
 
             # find "not enough times written" warning in log
-            warn_msg = "! Warning: while closing variable 0 (masso, table Omon)\n" \
-                       "! we noticed you wrote 4 time steps for the variable,\n" \
-                       "! but its time axis 0 (time) has 5 time steps"
+            warn_msg = "! Warning: while closing variable {} (masso, table Omon)\n" \
+                       "! we noticed you wrote {} time steps for the variable,\n" \
+                       "! but its time axis {} (time) has {} time steps" \
+                        .format(ivar, data_len, itime, time_len)
             remove_ansi = re.compile(r'\x1b\[[0-?]*[ -/]*[@-~]')
             with open(self.logfile) as f:
                 lines = f.readlines()
