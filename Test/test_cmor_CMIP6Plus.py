ae5afb6c5137dc678dd51248215e7834441b5040
mauzey1 <mauzey1@llnl.gov>
2024-01-19 11:37:42 -0800

    Update CMIP6Plus test

--- Test/test_cmor_CMIP6Plus.py
+++ Test/test_cmor_CMIP6Plus.py
@@ -5,13 +5,14 @@ import unittest
 class TestGrid(unittest.TestCase):
 
     def test_pass_only_axis_ids(self):
-        self.assertEquals(cmor.setup(inpath='mip-cmor-tables/Tables',
-                                     netcdf_file_action=cmor.CMOR_REPLACE),
-                          0)
-        self.assertEquals(cmor.dataset_json("Test/CMIP6Plus_user_input.json"),
-                          0)
+        self.assertEqual(
+            cmor.setup(inpath='mip-cmor-tables/Tables',
+                       netcdf_file_action=cmor.CMOR_REPLACE),
+            0)
+        self.assertEqual(cmor.dataset_json("Test/CMIP6Plus_user_input.json"),
+                         0)
 
-        self.assertEquals(cmor.load_table("MIP_OPmon.json"), 0)
+        self.assertEqual(cmor.load_table("MIP_OPmon.json"), 0)
         itim = cmor.axis(
             table_entry='time',
             units='months since 2010-1-1',
@@ -22,6 +23,10 @@ class TestGrid(unittest.TestCase):
 
         data = [280., ] * 12  # 12 months worth of data
 
-        self.assertEquals(cmor.write(ivar, data), 0)
+        self.assertEqual(cmor.write(ivar, data), 0)
 
-        self.assertEquals(cmor.close(), 0)
+        self.assertEqual(cmor.close(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
