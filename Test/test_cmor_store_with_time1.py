18993de3a1f67eb3c08a8f07d0388126d41add88
Chris Mauzey <mauzey1@llnl.gov>
2026-07-22 10:25:59 -0700

    Expand test coverage of storing with time bounds

--- Test/test_cmor_store_with_time1.py
+++ Test/test_cmor_store_with_time1.py
@@ -303,70 +303,277 @@ class TestStoreWithTime1(BaseCVsTest):
 
 class TestStoreWithTimeBounds(BaseCVsTest):
 
+    def setUp(self):
+        super().setUp()
+        self.ntime = 4
+        self.time_vals = numpy.array([15.0, 45.0, 75.0, 105.0], dtype="d")
+        self.time_bnds = numpy.array([0.0, 30.0, 60.0, 90.0, 120.0], dtype="d")
+        self.time_bnds_pairs = numpy.column_stack((self.time_bnds[:-1], self.time_bnds[1:]))
+        self.lat_vals = numpy.array([10.0, 20.0, 30.0], dtype="d")
+        self.lat_bnds = numpy.array([5.0, 15.0, 25.0, 35.0], dtype="d")
+        self.lon_vals = numpy.array([0.0, 90.0, 180.0, 270.0], dtype="d")
+        self.lon_bnds = numpy.array([-45.0, 45.0, 135.0, 225.0, 315.0], dtype="d")
+        self.lev_vals = numpy.array([0.92, 0.72, 0.50, 0.30, 0.10], dtype="d")
+        self.lev_bnds = numpy.array([1.00, 0.83, 0.61, 0.40, 0.20, 0.00], dtype="d")
+        self.nlev = len(self.lev_vals)
+        self.nlat = len(self.lat_vals)
+        self.nlon = len(self.lon_vals)
+
+        self.cl_data = numpy.arange(
+            self.ntime * self.nlev * self.nlat * self.nlon,
+            dtype="f",
+        ).reshape(self.ntime, self.nlev, self.nlat, self.nlon)
+        self.ps_data = (
+            97000.0 + numpy.arange(self.ntime * self.nlat * self.nlon, dtype="f")
+        ).reshape(self.ntime, self.nlat, self.nlon)
+
+    def _cmor_time_bounds_setup(self, input_path, time_axis_with_values=True):
+        cmor.setup(inpath=str(TABLES_PATH), netcdf_file_action=cmor.CMOR_REPLACE, logfile=self.tmpfile)
+        cmor.dataset_json(str(input_path))
+        cmor.load_table("CMIP7_atmos.json")
+
+        if time_axis_with_values:
+            time_axis = cmor.axis(
+                "time",
+                "days since 1979-01-01",
+                coord_vals=self.time_vals,
+                cell_bounds=self.time_bnds,
+            )
+        else:
+            time_axis = cmor.axis("time", "days since 1979-01-01")
+        lat_axis = cmor.axis(
+            "latitude",
+            "degrees_north",
+            coord_vals=self.lat_vals,
+            cell_bounds=self.lat_bnds,
+        )
+        lon_axis = cmor.axis(
+            "longitude",
+            "degrees_east",
+            coord_vals=self.lon_vals,
+            cell_bounds=self.lon_bnds,
+        )
+        lev_axis = cmor.axis(
+            "standard_hybrid_sigma",
+            "1",
+            coord_vals=self.lev_vals,
+            cell_bounds=self.lev_bnds,
+        )
+        cmor.zfactor(
+            zaxis_id=lev_axis,
+            zfactor_name="a",
+            axis_ids=[lev_axis],
+            zfactor_values=numpy.array([0.12, 0.22, 0.30, 0.20, 0.10], dtype="d"),
+            zfactor_bounds=numpy.array([0.06, 0.18, 0.26, 0.25, 0.15, 0.00], dtype="d"),
+        )
+        cmor.zfactor(
+            zaxis_id=lev_axis,
+            zfactor_name="b",
+            axis_ids=[lev_axis],
+            zfactor_values=numpy.array([0.80, 0.50, 0.20, 0.10, 0.00], dtype="d"),
+            zfactor_bounds=numpy.array([0.94, 0.65, 0.35, 0.15, 0.05, 0.00], dtype="d"),
+        )
+        cmor.zfactor(zaxis_id=lev_axis, zfactor_name="p0", units="Pa", zfactor_values=100000.0)
+        ps_id = cmor.zfactor(
+            zaxis_id=lev_axis,
+            zfactor_name="ps",
+            units="Pa",
+            axis_ids=[time_axis, lat_axis, lon_axis],
+        )
+        var_id = cmor.variable(
+            "cl_tavg-al-hxy-u",
+            "%",
+            [time_axis, lev_axis, lat_axis, lon_axis],
+            missing_value=1.0e20,
+        )
+
+        return var_id, ps_id
+
+    def _assert_bounded_time_output(self, filename):
+        with netCDF4.Dataset(filename) as dataset:
+            self.assertEqual(len(dataset.dimensions["time"]), self.ntime)
+            numpy.testing.assert_allclose(dataset.variables["time"][:], self.time_vals)
+            numpy.testing.assert_allclose(dataset.variables["time_bnds"][:], self.time_bnds_pairs)
+            self.assertEqual(dataset.variables["cl"].shape, (self.ntime, self.nlev, self.nlat, self.nlon))
+            self.assertEqual(dataset.variables["ps"].shape, (self.ntime, self.nlat, self.nlon))
+            numpy.testing.assert_allclose(
+                dataset.variables["ps"][:, 0, 0],
+                97000.0 + numpy.arange(0, self.ntime * self.nlat * self.nlon, self.nlat * self.nlon, dtype="f"),
+            )
+
     def test_store_with_bounded_time_axis_still_writes_expected_records(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             output_dir = Path(tmpdir)
             input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path)
 
-            ntime, nlev, nlat, nlon = 2, 5, 3, 4
-            time_vals = numpy.array([15.0, 45.0], dtype="d")
-            time_bnds = numpy.array([0.0, 30.0, 60.0], dtype="d")
-            lat_vals = numpy.array([10.0, 20.0, 30.0], dtype="d")
-            lat_bnds = numpy.array([5.0, 15.0, 25.0, 35.0], dtype="d")
-            lon_vals = numpy.array([0.0, 90.0, 180.0, 270.0], dtype="d")
-            lon_bnds = numpy.array([-45.0, 45.0, 135.0, 225.0, 315.0], dtype="d")
-            lev_vals = numpy.array([0.92, 0.72, 0.50, 0.30, 0.10], dtype="d")
-            lev_bnds = numpy.array([1.00, 0.83, 0.61, 0.40, 0.20, 0.00], dtype="d")
-
-            cmor.setup(inpath=str(TABLES_PATH), netcdf_file_action=cmor.CMOR_REPLACE, logfile=self.tmpfile)
-            cmor.dataset_json(str(input_path))
-            cmor.load_table("CMIP7_atmos.json")
-
-            time_axis = cmor.axis("time", "days since 1979-01-01", coord_vals=time_vals, cell_bounds=time_bnds)
-            lat_axis = cmor.axis("latitude", "degrees_north", coord_vals=lat_vals, cell_bounds=lat_bnds)
-            lon_axis = cmor.axis("longitude", "degrees_east", coord_vals=lon_vals, cell_bounds=lon_bnds)
-            lev_axis = cmor.axis("standard_hybrid_sigma", "1", coord_vals=lev_vals, cell_bounds=lev_bnds)
-            cmor.zfactor(
-                zaxis_id=lev_axis,
-                zfactor_name="a",
-                axis_ids=[lev_axis],
-                zfactor_values=numpy.array([0.12, 0.22, 0.30, 0.20, 0.10], dtype="d"),
-                zfactor_bounds=numpy.array([0.06, 0.18, 0.26, 0.25, 0.15, 0.00], dtype="d"),
-            )
-            cmor.zfactor(
-                zaxis_id=lev_axis,
-                zfactor_name="b",
-                axis_ids=[lev_axis],
-                zfactor_values=numpy.array([0.80, 0.50, 0.20, 0.10, 0.00], dtype="d"),
-                zfactor_bounds=numpy.array([0.94, 0.65, 0.35, 0.15, 0.05, 0.00], dtype="d"),
-            )
-            cmor.zfactor(zaxis_id=lev_axis, zfactor_name="p0", units="Pa", zfactor_values=100000.0)
-            ps_id = cmor.zfactor(
-                zaxis_id=lev_axis,
-                zfactor_name="ps",
-                units="Pa",
-                axis_ids=[time_axis, lat_axis, lon_axis],
-            )
-            var_id = cmor.variable(
-                "cl_tavg-al-hxy-u",
-                "%",
-                [time_axis, lev_axis, lat_axis, lon_axis],
-                missing_value=1.0e20,
-            )
-
-            data = numpy.arange(ntime * nlev * nlat * nlon, dtype="f").reshape(ntime, nlev, nlat, nlon)
-            ps_data = (97000.0 + numpy.arange(ntime * nlat * nlon, dtype="f")).reshape(ntime, nlat, nlon)
-
-            cmor.write(var_id, data)
-            cmor.write(ps_id, ps_data, store_with=var_id)
+            cmor.write(var_id, self.cl_data)
+            cmor.write(ps_id, self.ps_data, store_with=var_id)
             filename = cmor.close(var_id, file_name=True)
             cmor.close()
 
-            with netCDF4.Dataset(filename) as dataset:
-                self.assertEqual(len(dataset.dimensions["time"]), ntime)
-                numpy.testing.assert_allclose(dataset.variables["time"][:], time_vals)
-                self.assertEqual(dataset.variables["cl"].shape, (ntime, nlev, nlat, nlon))
-                self.assertEqual(dataset.variables["ps"].shape, (ntime, nlat, nlon))
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path)
+
+            for i in range(self.ntime):
+                cmor.write(var_id, self.cl_data[i:i + 1], ntimes_passed=1)
+                cmor.write(ps_id, self.ps_data[i:i + 1], ntimes_passed=1, store_with=var_id)
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_associated_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path)
+
+            cmor.write(var_id, self.cl_data)
+            for i in range(self.ntime):
+                cmor.write(ps_id, self.ps_data[i:i + 1], ntimes_passed=1, store_with=var_id)
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_variable_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path)
+
+            for i in range(self.ntime):
+                cmor.write(var_id, self.cl_data[i:i + 1], ntimes_passed=1)
+            cmor.write(ps_id, self.ps_data, store_with=var_id)
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_time_val_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path, time_axis_with_values=False)
+
+            cmor.write(var_id, self.cl_data, time_vals=self.time_vals, time_bnds=self.time_bnds)
+            cmor.write(ps_id, self.ps_data, time_vals=self.time_vals, time_bnds=self.time_bnds, store_with=var_id)
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_time_val_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path, time_axis_with_values=False)
+
+            for i in range(self.ntime):
+                cmor.write(
+                    var_id,
+                    self.cl_data[i:i + 1],
+                    time_vals=self.time_vals[i],
+                    time_bnds=self.time_bnds[i:i + 2],
+                    ntimes_passed=1,
+                )
+                cmor.write(
+                    ps_id,
+                    self.ps_data[i:i + 1],
+                    time_vals=self.time_vals[i],
+                    time_bnds=self.time_bnds[i:i + 2],
+                    ntimes_passed=1,
+                    store_with=var_id,
+                )
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_time_val_variable_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path, time_axis_with_values=False)
+
+            for i in range(self.ntime):
+                cmor.write(
+                    var_id,
+                    self.cl_data[i:i + 1],
+                    time_vals=self.time_vals[i],
+                    time_bnds=self.time_bnds[i:i + 2],
+                    ntimes_passed=1,
+                )
+            cmor.write(ps_id, self.ps_data, time_vals=self.time_vals, time_bnds=self.time_bnds, store_with=var_id)
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_axis_supports_chunked_time_val_associated_writes(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path, time_axis_with_values=False)
+
+            cmor.write(var_id, self.cl_data, time_vals=self.time_vals, time_bnds=self.time_bnds)
+            for i in range(self.ntime):
+                cmor.write(
+                    ps_id,
+                    self.ps_data[i:i + 1],
+                    time_vals=self.time_vals[i],
+                    time_bnds=self.time_bnds[i:i + 2],
+                    ntimes_passed=1,
+                    store_with=var_id,
+                )
+            filename = cmor.close(var_id, file_name=True)
+            cmor.close()
+
+            self._assert_bounded_time_output(filename)
+
+    def test_store_with_bounded_time_without_axis_or_passed_time_values_raises(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path, time_axis_with_values=False)
+
+            for i in range(self.ntime):
+                cmor.write(
+                    var_id,
+                    self.cl_data[i:i + 1],
+                    time_vals=self.time_vals[i],
+                    time_bnds=self.time_bnds[i:i + 2],
+                    ntimes_passed=1,
+                )
+            with self.assertRaises(cmor.CMORError):
+                cmor.write(ps_id, self.ps_data, store_with=var_id)
+            self.assertCV(
+                "variable 'ps' (table: atmos) you are passing 0 times but no "
+                "values and you did not define them via cmor_axis"
+            )
+
+    def test_store_with_bounded_time_associated_write_past_parent_time_raises(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            output_dir = Path(tmpdir)
+            input_path = write_user_input(output_dir, "mon")
+            var_id, ps_id = self._cmor_time_bounds_setup(input_path)
+
+            cmor.write(var_id, self.cl_data[:1], ntimes_passed=1)
+            with self.assertRaises(cmor.CMORError):
+                cmor.write(ps_id, self.ps_data, store_with=var_id)
+
+            self.assertCV(
+                "You are trying to write 4 time steps for associated variable "
+                "'ps' (table: atmos), starting at time step 0, but associated "
+                "variable 'cl' only has 1 time steps available.",
+                number_of_lines_to_scan=4,
+            )
 
 
 if __name__ == "__main__":
